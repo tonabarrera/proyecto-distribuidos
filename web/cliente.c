bdc3cac69bc18b45cb5284fe3ebffeddfc118c2f
Tonatihu <carlostonatihu@gmail.com>
2018-06-19 11:54:43 -0500

    Nada

--- web/cliente.c
+++ web/cliente.c
@@ -3,7 +3,7 @@
 #include "mongoose.h"
 
 
-static const char *s_http_port = "8888";
+static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
 
 
@@ -11,13 +11,20 @@ static void ev_handler(struct mg_connection *nc, int ev, void *p) {
     char RFC[13] = {0};
     if (ev == MG_EV_HTTP_REQUEST) {
         struct http_message *mensaje =(struct http_message *) p;
-        printf("LA respuesta: %s\n", mensaje->method);
-        if (mensaje->uri.p[1] == 'o') {
+        printf("LA url%s TERMINA\n", mensaje->uri);
+        if (mensaje->uri.p[1] == 'r') {
             printf("%s\n", "CONSULTA");
             memcpy(RFC, mensaje->uri.p+14, 13);
             printf("%s\n", RFC);
-        }
-        mg_printf(nc, "HTTP/1.0 200 OK\r\n\r\n[I am Hello1]");
+            //mg_serve_http(nc, mensaje, s_http_server_opts);
+            mg_printf(nc, "HTTP/1.1 200 OK\r\n"
+            "Cache: no-cache\r\n"
+            "Content-Type: application/json\r\n"
+            "Content-Length: %d\r\n"
+            "\r\n");
+            //mg_serve_http(nc, mensaje, s_http_server_opts);
+        } else
+            mg_serve_http(nc, mensaje, s_http_server_opts);
     }
 }
 int main(void) {
@@ -32,6 +39,8 @@ int main(void) {
     }
     // Set up HTTP server parameters
     mg_set_protocol_http_websocket(nc);
+    s_http_server_opts.document_root = "."; // Serve current directory
+    s_http_server_opts.enable_directory_listing = "yes";
     for (;;) {
         mg_mgr_poll(&mgr, 1000);
     }
